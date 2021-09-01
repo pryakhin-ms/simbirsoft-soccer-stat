8e4dd834cbabf5656f47fa924f021a164b4a0bee
PryakhinMS <pryakhin.ms@gmail.com>
2021-09-01 21:24:31 +0400

    Исправлено изменение состояния: теперь в состояние записывается массив лиг, а не данные полученные после запроса

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,13 @@ import LeaguesTab from './LeaguesTab';
 function App() {
   const [leaguesData, setLeaguesData] = useState([]);
   useEffect(async () => {
-    const response = await axios({
+    const { data: { competitions } } = await axios({
       headers: { 'X-Auth-Token': 'a23f85ec349c4bb0ae562a4ba2b748a8' },
       method: 'get',
       url: 'http://api.football-data.org/v2/competitions?plan=TIER_ONE',
     });
-    console.log(response);
-    setLeaguesData(response);
+    console.log(competitions, leaguesData);
+    setLeaguesData(competitions);
   }, []);
   return (
     <>
