66257e98f166cdaf4cf7c8ffb4560be4a8126e3b
PryakhinMS <pryakhin.ms@gmail.com>
2021-09-01 20:38:38 +0400

    Добавлены хуки состояния и эффекта. В хуке эффекта реализован запрос списка футбольных лиг

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import Sidebar from './Sidebar';
 import LeaguesTab from './LeaguesTab';
 
 function App() {
+  const [leaguesData, setLeaguesData] = useState([]);
+  useEffect(async () => {
+    const response = await axios({
+      headers: { 'X-Auth-Token': 'a23f85ec349c4bb0ae562a4ba2b748a8' },
+      method: 'get',
+      url: 'http://api.football-data.org/v2/competitions?plan=TIER_ONE',
+    });
+    console.log(response);
+    setLeaguesData(response);
+  }, []);
   return (
     <>
       <Sidebar />
