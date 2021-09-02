50a4e8b494b181d0e773b4381d8f5d8d1730253e
PryakhinMS <pryakhin.ms@gmail.com>
2021-09-02 20:40:41 +0400

    Откорректирована вёрстка сайдбара

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Sidebar() {
   return (
-    <div style={{ display: 'flex', height: '100vh' }}>
+    <div style={{ display: 'flex', height: 'auto' }}>
       <div className="d-flex flex-column flex-shrink-0 p-3 bg-light" style={{ width: '230px' }}>
         <a href="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
           <span className="fs-4">SoccerStat</span>
