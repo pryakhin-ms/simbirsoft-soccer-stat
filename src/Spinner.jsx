c84147d02403e7258de36a71207a01a2868c45f8
PryakhinMS <pryakhin.ms@gmail.com>
2021-09-03 23:04:17 +0400

    Добавлен компонент Spinner.jsx, который будет отображаться во время загрузки данных

---
+++ src/Spinner.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Spinner() {
+  return (
+    <div className="text-center">
+      <div className="spinner-border" role="status" />
+    </div>
+  );
+}
+
+export default Spinner;
