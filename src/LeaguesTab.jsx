d711d222f7d3cb7e681a61b9a42d16ef7d354274
PryakhinMS <pryakhin.ms@gmail.com>
2021-09-03 23:08:30 +0400

    В компонент LeaguesTab была проброшена переменная uiState, добавлена логика отображения спиннера

--- src/LeaguesTab.jsx
+++ src/LeaguesTab.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import Item from './Item';
+import Spinner from './Spinner';
 
 function LeaguesTab(props) {
-  const { leaguesList } = props;
+  const { leaguesList, uiState } = props;
   return (
     <div className="container">
       Список лиг
       <hr />
       <div className="row row-cols-1 row-cols-sm-1 row-cols-md-2 row-cols-xl-3 g-3">
+        {uiState === 'loading' && <Spinner />}
         {leaguesList.map((league) => <Item league={league} key={league.id} />)}
       </div>
     </div>
