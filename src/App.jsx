648e16424b8582c7bbb30677c6e5e3a86e4f1492
PryakhinMS <pryakhin.ms@gmail.com>
2021-09-05 21:41:35 +0400

    В функцию смены активного таба добавлена логика для загрузки данных по необходимости

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import fetchLeagues from './api/fetchLeagues';
+import fetchTeams from './api/fetchTeams';
 // import axios from 'axios';
 import Sidebar from './Sidebar';
 import DataTab from './DataTab';
@@ -8,6 +9,7 @@ function App() {
   const [uiState, setUiState] = useState('init');
   const [activeTab, setTab] = useState('leagues');
   const [leaguesList, setLeaguesData] = useState([]);
+  const [teamsList, setTeamsData] = useState([]);
   useEffect(async () => {
     setUiState('loading');
     try {
@@ -18,9 +20,27 @@ function App() {
     }
   }, []);
 
-  const changeActiveTab = (e) => {
+  const changeActiveTab = async (e) => {
     e.preventDefault();
-    setTab(activeTab === 'leagues' ? 'teams' : 'leagues');
+    const newTab = activeTab === 'leagues' ? 'teams' : 'leagues';
+    const setLeaguesOrTeams = async () => {
+      const activeData = newTab === 'leagues' ? leaguesList : teamsList;
+      if (activeData.length > 0) {
+        console.log(activeData);
+        return;
+      }
+      const fetchFunction = newTab === 'leagues' ? fetchLeagues : fetchTeams;
+      const setStateFunction = newTab === 'leagues' ? setLeaguesData : setTeamsData;
+      setUiState('loading');
+      try {
+        setStateFunction(await fetchFunction());
+        setUiState('init');
+      } catch {
+        setUiState('error');
+      }
+    };
+    setTab(newTab);
+    setLeaguesOrTeams();
   };
 
   return (
